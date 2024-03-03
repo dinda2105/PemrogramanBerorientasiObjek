5f88e991da9dd8557dc395686fd55e756854f4da
dinda2105 <161622589+dinda2105@users.noreply.github.com>
2024-03-03 18:47:25 +0700

    Delete mahasiswa directory

--- mahasiswa/dist/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"FromBiodata","l":"FromBiodata()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"FromBiodata","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
